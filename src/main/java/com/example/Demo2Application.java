997f9a78e967b20b187e556b2c432aacc1acfb7a
zxf-god <49678280+zxf-god@users.noreply.github.com>
2019-04-17 20:03:30 +0800

    Update Demo2Application.java

--- src/main/java/com/example/Demo2Application.java
+++ src/main/java/com/example/Demo2Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo2Application {
 
     public static void main(String[] args) {
-        System.out.println("1111111111111");
+        System.out.println("2222222221");
         SpringApplication.run(Demo2Application.class, args);
     }
 
