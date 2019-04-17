97edcb63dd31f943917ed4869601759c425685dc
zxf-god <z931871506@163.com>
2019-04-17 19:59:02 +0800

    this is omdfghjk

--- src/main/java/com/example/Demo2Application.java
+++ src/main/java/com/example/Demo2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo2Application {
 
     public static void main(String[] args) {
+        System.out.println("1111111111111");
         SpringApplication.run(Demo2Application.class, args);
     }
 
